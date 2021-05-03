7104541128325838088f754e8de0d3700572e95b
RakibRahman <md.rr.talukder@gmail.com>
2021-05-04 01:54:47 +0600

    code refactored

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
 const output = document.getElementById("output");
 const btn = document.querySelector("#btn");
 
-(async function () {
-  const fetchAPI = fetch(`https://bn-hadith-api.herokuapp.com/hadiths`);
-  const response = await fetchAPI;
-  const data = await response.json();
-  console.log(data.length);
-})();
+async function generateHadith() {
+  const api = "https://bn-hadith-api.herokuapp.com/hadiths";
+  const url = fetch(`https://bn-hadith-api.herokuapp.com/hadiths`);
+  const response = await url;
+  const idNumbers = await response.json();
+  const index = idNumbers.length;
+  const random = Math.floor(Math.random() * index) + 1;
 
-function generateHadith() {
-  const random = Math.floor(Math.random() * 30) + 1;
-  const fetchAPI = fetch(
-    `https://bn-hadith-api.herokuapp.com/hadiths/${random}`
-  );
-  fetchAPI
-    .then((response) => response.json())
-    .then((data) => {
-      output.innerHTML = `
-      <p class="name">${data.name}</p>
-      <p class="description">${data.description}</p>
-      <p class="info">হাদিসের রেফারেন্সঃ ${data.numbers}</p>
-      <p class="info">হাদিসের মানঃ <span class="grade">${data.grade}</span></p>
-      
-      `;
-      const gradeBG = document.querySelector(".grade");
-      if (data.grade === "জাল হাদিস") {
-        gradeBG.style.backgroundColor = "#f14";
-        console.log("red");
-      }
-    });
+  const fetchHadith = await fetch(`${api}/${random}`);
+  const data = await fetchHadith.json();
+  output.innerHTML = `
+  <p class="name">${data.name}</p>
+  <p class="description">${data.description}</p>
+  <p class="info">হাদিসের রেফারেন্সঃ ${data.references}</p>
+  <p class="info">হাদিসের মানঃ <span class="grade">${data.grade}</span></p>
+  
+  `;
+
+  const gradeBG = document.querySelector(".grade");
+  if (
+    data.grade === "জাল হাদিস" ||
+    data.grade === "দুর্বল হাদিস" ||
+    data.grade === "মুনকার"
+  ) {
+    gradeBG.style.backgroundColor = "#f85b24";
+  } else if (data.grade === "নির্ণীত নয়") {
+    gradeBG.style.backgroundColor = "#f0ad4e";
+  }
 }
 
 function init() {
   generateHadith();
-  // randomId();
   btn.addEventListener("click", generateHadith);
 }
 init();
